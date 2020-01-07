2af4153de37b20b8616326ebdcb3207324f60ad7
unknown <raymondlevengood@gmail.com>
2020-01-07 14:35:17 -0500

    got map method working. working on router/css/home module

--- dnd-util-react/src/App.js
+++ dnd-util-react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css'
+import { BrowserRouter as Router, Switch, ROute, Link } from 'react-router-dom';
+import Home from './Home.js'
 // import { useEffect } from 'react'
 
 import SpellList from './SpellList/SpellList.js'
@@ -11,20 +13,36 @@ function App() {
 
   return (
     <div className="fullApp">
-      <header className='header'>
+      
+      <nav className='header'>
          <ul>
           <li className="appTitle"><a href="home.asp">DnD-Utils</a></li>
-          <li><a href="databases.asp">Databases</a></li>
-          <li><a href="tools.asp">Tools</a></li>
+          <li><a href="databases.asp">Databases</a>
+            <ul class="dropdown">
+              <li><a href="#">Sub-1</a></li>
+              <li><a href="#">Sub-2</a></li>
+              <li><a href="#">Sub-3</a></li>
+            </ul>
+          </li>
+          <li><a href="tools.asp">Tools</a>
+            <ul class="dropdown">
+              <li><a href="#">Sub-1</a></li>
+              <li><a href="#">Sub-2</a></li>
+              <li><a href="#">Sub-3</a></li>
+            </ul>
+          </li>
           <li><a href="about.asp">About</a></li>
         </ul> 
-      </header>
+      </nav> 
+      
       <div className='secondHeader'>
         <p>secondHeader</p>
       </div>
       <div className='body'>
+        <Home />
         <SpellList />
       </div>
+      <div className='footer' />
     </div>
   );
 }
--- dnd-util-react/src/SpellList/SpellList.js
+++ dnd-util-react/src/SpellList/SpellList.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./SpellList.css";
 
-import SingleSpell from "./SingleSpell/SingleSpell.js";
+// import SingleSpell from "./SingleSpell/SingleSpell.js";
 
 function SpellList() {
 
-   let spellDataF = [];
+  const [spells, setSpells] = useState([]);
 
-   useEffect(() => {
+
+  useEffect(() => {
     fetch("http://www.dnd5eapi.co/api/spells")
       .then(res => res.json())
-      .then(spellData => {
-        spellDataF = spellData.results.map((spells, index) => {
-          console.log(spellDataF)
-          return <div>{spells.name}</div>;
-        });
-      });
-  });
-//   console.log(spellDataF)
-  return (
-    <>
+      .then(spellData => setSpells(spellData.results))
+  }, []);
 
-    </>
+  const renderSpells = spells.map( (spell, index) => <button className='singleSpell' key={spell.index}>{spell.name}</button> ) 
+
+
+  // console.log(spells)
+  return (
+    <div className='mainDisplay'>
+      {renderSpells}
+    </div>
   );
 }
 
